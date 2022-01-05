84dbfae7f5c82d9e5615842b205e601e9d50f8bf
Krish Matta <self@krishxmatta.dev>
2022-01-04 22:34:24 -0500

    Fix formatting issues

--- axe/cli.py
+++ axe/cli.py
@@ -5,7 +5,7 @@ import requests
 @click.command()
 @click.argument("question")
 def cli(question):
-    click.echo(parse_page(query(search(question))))
+    click.echo(query(search(question)))
 
 def search(question):
     parameters = {
@@ -35,24 +35,11 @@ def query(title):
         "action": "query",
         "format": "json",
         "prop": "extracts",
+        "exintro": 1,
+        "explaintext": 1,
+        "redirects": 1,
         "titles": title,
     }
-    
+
     response = requests.get("https://en.wikipedia.org/w/api.php", params=parameters)
-    return list(response.json()["query"]["pages"].values())[0]["extract"]
-
-def parse_page(extract):
-    extract = extract.split('</p>')
-
-    tags = re.compile("<.*?>")
-    extract = map(lambda x: re.sub(tags, " ", x), extract)
-
-    extract = next(filter(lambda x: x and not x.isspace(), extract)) # Have to use this method rather than relying on HTML paragaph tags to extract the first paragraph due to malformed HTML
-
-    whitespace = re.compile(" +")
-    extract = re.sub(whitespace, " ", extract)
-
-    breaks = re.compile("\n")
-    extract = re.sub(breaks, "", extract)
-
-    return extract
+    return list(response.json()["query"]["pages"].values())[0]["extract"].split("\n")[0]
