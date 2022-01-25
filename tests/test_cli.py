926075278906018103a85ef4fb672ca2da5fb149
Krish Matta <self@krishxmatta.dev>
2022-01-24 22:55:22 -0500

    Implement unit tests

---
+++ tests/test_cli.py
@@ -0,0 +1,4 @@
+from axe.cli import query
+
+def test_query():
+    assert query("fish") == "Fish are aquatic, craniate, gill-bearing animals that lack limbs with digits. Included in this definition are the living hagfish, lampreys, and cartilaginous and bony fish as well as various extinct related groups. Around 99% of living fish species are ray-finned fish, belonging to the class Actinopterygii, with over 95% belonging to the teleost subgrouping."
